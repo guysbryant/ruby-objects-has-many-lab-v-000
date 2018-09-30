cf9621105cbc95eee6517a4d388ca4d87fc85276
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-30 02:40:24 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,13 @@
 class Artist
   attr_accessor :name, :songs
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+    songs << song
+  end
 end
